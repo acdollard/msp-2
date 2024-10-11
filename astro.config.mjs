5ad716c17930646cf1c2905859fe4cfcf369b7a7
Alexander Dollard <alexdollard141@gmail.com>
2024-10-11 09:12:35 -0500

    initial config sanity + astro

---
+++ astro.config.mjs
@@ -0,0 +1,19 @@
+// astro.config.mjs
+import { defineConfig } from "astro/config";
+import { sanityIntegration } from "@sanity/astro";
+import react from "@astrojs/react";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    sanityIntegration({
+      projectId: "tzo5r4as",
+      dataset: "production",
+      useCdn: false, // See note on using the CDN
+      apiVersion: "2024-07-24", // insert the current date to access the latest version of the API
+      studioBasePath: "/studio", // If you want to access the Studio on a route
+    }),
+    react(),
+  ],
+  output: "server",
+});
